fdb8859bd80ece56a7e13125776a7349a68a47b3
hyooniy <jkas8233@naver.com>
2023-06-23 16:12:30 +0900

    프로젝트 완성

--- js/index.js
+++ js/index.js
@@ -9,16 +9,20 @@ $(function(){
     }
     else if(windowW > 980 && windowW <= 1154){
         nav();
-        asideNav();
     }
     else if(windowW > 580 && windowW <= 979){
         tNav()
+        gallery();
     }
     else if(windowW <= 579){
-        asideNav();
-        nav();
+       tNav();
+       gallery();
+       form()
     }
-
+    //화면의 크기변경의 resize
+    $(window).on('resize',function(){
+        window.location.reload();
+    })
     //함수 만들기
     function nav(){
         $('nav li>a').on('click',function(){
@@ -51,4 +55,80 @@ $(function(){
     }
     function asideNav(){}
 
+    //갤러리
+    function gallery(){
+        let figureW = $('#all figure').width();
+        $('#all>figure:last').prependTo('#all')
+        $('#all').css('margin-left','-'+figureW+'px');
+        $('#gallery .prev').on('click',function(){
+            $('#all').animate({marginLeft: '+=' + figureW + 'px'},600,function(){
+                $('#all>figure:last').prependTo('#all')
+                $('#all').css('margin-left','-'+figureW+'px');
+            })
+        });
+        $('#gallery .next').on('click',function(){
+            $('#all').animate({marginLeft: '-=' + figureW + 'px'},600,function(){
+            $('#all>figure:first').appendTo('#all')
+            $('#all').css('margin-left','-'+figureW+'px'); 
+            })
+           
+        })
+
+    }
+})
+//form 모바일
+ function form(){
+    const $liForm = $('#box04 li>input, #box04 li>textarea');
+    $liForm.removeAttr('placeholder');
+    $liForm.on('focus',function(){
+        $(this).prev('label').fadeOut(400);
+      });
+
+      $liForm.on('blur',function(){
+        const str = $(this).val();
+        if(str === ''){
+        $(this).prev('label').fadeIn(400);
+        }
+      })
+ }
+
+ //객체만들기
+
+function Modal(title,pic,year,program,url,text){
+    this.title = title;
+    this.pic = pic;
+    this.year = year;
+    this.program =  program;
+    this.url = url
+    this.text = text
+}
+Modal.prototype.action =function(){
+    document.querySelector('#modal h4').innerHTML = this.title;
+    document.querySelector('#modal figure>img').setAttribute('src',this.pic);
+    document.querySelector('#modal figure>figcation').innerHTML = this.title;
+    document.querySelector('#modal dl>dd:nth-child(2)').innerHTML =this.year
+    document.querySelector('#modal dl>dd:nth-child(4)').innerHTML = this.program
+    document.querySelector('#modal dl>dd>a').setAttribute('href', this.url)
+    document.querySelector('#modal dl>dd>a').innerHTML = this.url;
+    document.querySelector('#modal dl>dd:nth-child(8)').innerHTML = this.text
+}
+let mymodal = [
+    new Modal('title01','./images/pic01.png','2001','프로그램1','http://www.a1.com','text01'),
+    new Modal('title02','./images/pic02.png','2002','프로그램2','http://www.a12.com','text02'),
+    new Modal('title03','./images/pic03.png','2003','프로그램3','http://www.a13.com','text03'),
+    new Modal('title04','./images/pic04.png','2004','프로그램4','http://www.a14.com','text04'),
+    new Modal('title05','./images/pic05.png','2005','프로그램5','http://www.a15.com','text05'),
+    new Modal('title06','./images/pic06.png','2006','프로그램6','http://www.a16.com','text06'),
+]
+const btn = document.querySelectorAll('#box03 #all>figure');
+const close = document.querySelector('#modal .close');
+
+btn.forEach(function(item,index){
+    item.addEventListener('click',function(){
+        document.querySelector('#modal').style.display = 'block';
+        mymodal[index].action();
+    })
+})
+close.addEventListener('click',function(){
+    document.querySelector('#modal').style.display = 'none';
 })
